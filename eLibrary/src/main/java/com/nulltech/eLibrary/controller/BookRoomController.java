39eec77c16bf8fa6cf4a5f491d1fea26dd8e4dac
Raj Chanpura <Raj Chanpura@DESKTOP-R9IJ8EO>
2019-11-10 16:38:43 +0530

    First commit

---
+++ eLibrary/src/main/java/com/nulltech/eLibrary/controller/BookRoomController.java
@@ -0,0 +1,35 @@
+package com.nulltech.eLibrary.controller;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.nulltech.eLibrary.dto.BookRoomDTO;
+import com.nulltech.eLibrary.service.IBookRoomServices;
+
+import com.nulltech.eLibrary.service.Impl.BookRoomServiceImpls;
+
+@Controller
+public class BookRoomController
+{
+	@Autowired
+	private BookRoomServiceImpls bookRoomService;
+	
+	@Autowired
+	private IBookRoomServices Ibookroom;
+	
+	@GetMapping("/bookRoom")
+	public void home()
+	{
+		BookRoomDTO bookroom = new BookRoomDTO();
+		bookroom.setAdditionalRequirements("yes");
+		bookroom.setBookingStatus(2);
+		bookroom.setEndDate(new Date());
+		bookroom.setNumberOfPersons(4);
+		bookroom.setStartTime(new Date());
+		
+		bookRoomService.addBookRoom(bookroom);
+	}
+}
